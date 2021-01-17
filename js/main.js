37f1bcf03e485cb86237ef2d2b7b52c406cb0546
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-17 22:19:10 +0100

    testing-ES6-classes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
- 
+
+// Classing test 
+class Human {
+    species = "human";
+}
+
+class Person extends Human {
+    name = "Domenico";
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+// Invoke the new object 
+const person = new Person();
+person.printMyName();
+console.log(person.species);
